bdf6dc12d94f9e28a3dcfd15e8fc23f6a45a1a64
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2023-04-07 00:08:51 -0300

    Apply: Label encoder to variables Geography and Gender

--- 1 - Construindo redes neurais artificiais/churn.py
+++ 1 - Construindo redes neurais artificiais/churn.py
@@ -7,6 +7,8 @@ Created on Thu Apr  6 23:46:44 2023
 
 # Importando bibliotecas
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import  make_column_transformer
 
 #importando dados
 dataset = pd.read_csv('Churn_Modelling.csv')
@@ -14,3 +16,10 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 # Separando dados independentes e dependente
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
+# Encoders - Categóricos para numéricos
+labelEncoder_x_1 = LabelEncoder()
+X[:,1] = labelEncoder_x_1.fit_transform(X[:,1])
+
+labelEncoder_x_2 = LabelEncoder()
+X[:,2] = labelEncoder_x_1.fit_transform(X[:,2])
