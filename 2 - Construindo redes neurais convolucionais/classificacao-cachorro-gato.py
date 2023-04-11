434c090a0560bb0c84a81b3bb39abfe222c85e82
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2023-04-11 08:39:36 -0300

    Add: criando rede convolucional de classificação cachorro/gato

---
+++ 2 - Construindo redes neurais convolucionais/classificacao-cachorro-gato.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 07:29:08 2023
+
+@author: carlo
+"""
+
+# Importando bibliotecas
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Criando o classificador
+classifier = Sequential()
+
+# Parâmetros
+
+# Número de filtros, detectores de características
+filtros = 32
+# Kernel
+kernel = (3, 3)
+# Formato do input
+input_shape = (64, 64, 3)
+
+classifier.add(
+    Conv2D(filtros, kernel, input_shape=input_shape, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Flatten())
+
+# Como experimento, definimos 128 neurônios
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+# Compilando o modelo
+classifier.compile(
+    optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Pré-processamento das imagens
+train_dataimg = ImageDataGenerator(
+    rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test_dataimg = ImageDataGenerator(rescale=1./255)
+
+# Carregar os dados de treino
+train_set = train_dataimg.flow_from_directory(
+    'data/training_set', target_size=(64, 64), batch_size=32, class_mode='binary')
+
+# Carregando dados de teste
+test_set = test_dataimg.flow_from_directory(
+    'data/test_set',  target_size=(64, 64), batch_size=32, class_mode='binary')
+
+# Treinando a rede neural
+classifier.fit(train_set, steps_per_epoch = 8000//32, epochs = 15, validation_data = test_set, validation_steps = 2000//32)
