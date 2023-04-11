35656c641cb72db24854c9cca3c02aa88768a4c0
Carlos Alberto Silva Júnior <carlosalbertospace@hotmail.com>
2023-04-10 22:54:56 -0300

    Update churn-classification-with-keras-featurespace.py

--- 1 - Construindo redes neurais artificiais/churn-classification-with-keras-featurespace.py
+++ 1 - Construindo redes neurais artificiais/churn-classification-with-keras-featurespace.py
@@ -5,7 +5,7 @@ Created on Mon Apr 10 07:51:33 2023
 @author: carlo
 """
 
-# DATA PREPARATION
+# LOADING MODULES
 
 import numpy as np
 import pandas as pd
@@ -21,6 +21,8 @@ pd.options.display.float_format = '{:.5f}'.format
 
 # Importing data
 dataframe = pd.read_csv('./data/Churn_Modelling.csv')
+
+# DATA PREPARATION
 
 # Split independent and dependent data
 X = dataframe.iloc[:, 3:13]
@@ -49,6 +51,40 @@ train_ds = train_ds.batch(32)
 test_ds = test_ds.batch(32)
 
 # Configuring a FeatureSpace
+feature_space = FeatureSpace(
+    features={
+        "Tenure": FeatureSpace.float_normalized(),
+        "Age": FeatureSpace.float_discretized(num_bins=20),
+        "CreditScore": FeatureSpace.float_normalized(),
+        "Balance": FeatureSpace.float_normalized(),
+        "NumOfProducts": FeatureSpace.float_normalized(),
+        "EstimatedSalary": FeatureSpace.float_normalized(),
+        "Geography": FeatureSpace.string_categorical(),
+        "Gender": FeatureSpace.string_categorical(),
+        "HasCrCard": FeatureSpace.integer_categorical(),
+        "IsActiveMember": FeatureSpace.integer_categorical(),
+    },
+    crosses=[
+        FeatureSpace.cross(feature_names=("Age", "Gender"), crossing_dim=64),
+    ],
+    output_mode="concat",
+)
 
+#Applying FeatureSpace
+train_ds_with_no_labels = train_ds.map(lambda x, _: x)
+feature_space.adapt(train_ds_with_no_labels)
 
+# Creating a training and validation dataset of preprocessed batches
+preprocessed_train_ds = train_ds.map(
+    lambda x, y: (feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
+)
+preprocessed_train_ds = preprocessed_train_ds.prefetch(tf.data.AUTOTUNE)
 
+preprocessed_val_ds = test_ds.map(
+    lambda x, y: (feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
+)
+preprocessed_val_ds = preprocessed_val_ds.prefetch(tf.data.AUTOTUNE)
+
+# BUILDING A MODEL
+dict_inputs = feature_space.get_inputs()
+encoded_features = feature_space.get_encoded_features()
